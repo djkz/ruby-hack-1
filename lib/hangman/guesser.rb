5f8aa9efb574348f7cfa4a425970640e74f537a6
Timur Kunayev <timur.kunayev@gmail.com>
2012-12-30 14:58:10 -0700

    guesser class

--- lib/hangman/guesser.rb
+++ lib/hangman/guesser.rb
@@ -1,6 +1,24 @@
 class Guesser
 	
 	def self.guess(word, guessed_word, guessed_letter)
+      combine(guessed_word, match(word,guessed_letter), guessed_letter)
 		
 	end
+
+    def self.match(word,letter)
+      word.split("").map{|l| l == letter}
+    end
+
+    def self.combine(word,key,new_letter)
+      word_array = word.split('')
+      result = ""
+      word_array.each_with_index do |old_letter,index|
+        if key[index]
+          result += new_letter
+        else
+          result += old_letter
+        end
+      end
+      result
+    end
 end
--- spec/lib/hangman/guesser_spec.rb
+++ spec/lib/hangman/guesser_spec.rb
@@ -4,15 +4,23 @@ describe Guesser do
 
   context "will take word, guessed_word and a letter" do
 
-		it "letter is in the word once" do
-          Guesser.guess("word","_ _ _ _", w).should eq "w _ _ _"
-		end
+    it "will return array of true/false value for matching letters" do
+      Guesser.match("word","w").should == [true,false,false,false]
+    end
 
-		it "letter is not in the word" do
-		end
+    it "will combine array of booleans with a word using letter" do
+      Guesser.combine("____",[true,false,false,false],"w").should == "w___"
+    end
 
-		it "letter is in the word multiple times" do
-		end
-	end
+    it "letter is in the word once" do
+      Guesser.guess("word","____", "w").should == "w___"
+    end
+
+    it "letter is not in the word" do
+    end
+
+    it "letter is in the word multiple times" do
+    end
+  end
 
 end
